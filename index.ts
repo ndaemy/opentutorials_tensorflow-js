22868983cc64e0c54315454d58c9b7bf638bb37f
soultree-fly <yuyaebean@gmail.com>
2021-01-10 01:12:14 +0900

    Feat: Create lemonade model

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
 import * as tf from '@tensorflow/tfjs-node';
 
-console.log(tf);
+const temperatures = [20, 21, 22, 23];
+const salesRate = [40, 42, 44, 46];
+
+const src = tf.tensor(temperatures);
+const dst = tf.tensor(salesRate);
+
+const X = tf.input({ shape: [1] });
+const Y = tf.layers.dense({ units: 1 }).apply(X);
+const model = tf.model({ inputs: X, outputs: Y as tf.SymbolicTensor });
+const compileParam = {
+  optimizer: tf.train.adam(),
+  loss: tf.losses.meanSquaredError,
+};
+model.compile(compileParam);
+
+function runFit(times?: number) {
+  const fitParam = { epochs: times ? times * 100 : 100 };
+  model.fit(src, dst, fitParam).then(result => {
+    const predictData = model.predict(src);
+    (predictData as tf.Tensor<tf.Rank>).print();
+  });
+}
